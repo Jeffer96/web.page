1978d70c3e390ecff601afdefe2b3c3476353f94
Jefferson Castañeda <jefferson.castaneda@mail.escuelaing.edu.co>
2019-02-08 07:36:07 -0500

    avance 2019 02 08

--- resources/js/event.js
+++ resources/js/event.js
@@ -1,25 +1,45 @@
+var menuSelected;var dc;var blocked = true;var links = {'LIDX':"inicio.html",'LSRV':"servicios.html",'LACR':"acerca.html",'LCTO':"contacto.html",'portal':"portal.airefrescodecolombia.com"};
 
-var menuItemSelected;
-
-function onLoad(){
-	menuItemSelected = "Inicio";
-	loadLink("inicio.html");
+function loaded(){
+	dc = document.getElementById("dynamicContent");
+	menuSelected = "LIDX";
+	loadLink(links[menuSelected]);
+	//document.getElementById("dynamicContent").onload = animate();
 }
 
-function goTo(id, urlPage){
-	if (menuItemSelected!=id){
-		document.getElementById(menuItemSelected).classList.remove("selected");
-		document.getElementById(menuItemSelected).classList.add("unSelected");
-		menuItemSelected = id.toString();
-		document.getElementById(menuItemSelected).classList.remove("unSelected");
-		document.getElementById(menuItemSelected).classList.add("selected");
-		loadLink(urlPage);
+function selected(id){
+	if (menuSelected!=id && !blocked){ 
+		document.getElementById(menuSelected).setAttribute("class","selected");
+		document.getElementById(menuSelected).setAttribute("class","unSelected");
+		menuSelected = id;
+		document.getElementById(menuSelected).setAttribute("class","unSelected");
+		document.getElementById(menuSelected).setAttribute("class","selected");
+		loadLink(links[menuSelected]);
 	}
+		
 }
 
-function loadLink(urlPage){
-	var innerContent = '<object id="content" type="text/html" data="'+urlPage+'" ></object>';
-    document.getElementById("dynamicContent").innerHTML=innerContent;
-	//document.getElementById("dynamicContent").style.height = document.getElementById("content").scrollHeight;
-	alert(document.getElementById("dynamicContent").scrollHeight);
+function loadLink(url){
+	blocked = true;
+	dc.style.display = "none";
+	dc.setAttribute("src",url);
+	dc.onload = function(){
+		dc.style.display = "block";
+		var hv = dc.contentWindow.document.body.scrollHeight;
+		var counter = hv/100;
+		var myTimer = setInterval(animate, 20);
+		function animate () {
+			if (counter <  hv ){
+				dc.style.height = counter;
+				counter = counter + 10;
+			}else {
+				clearInterval(myTimer);
+				blocked = false;
+			}
+		}
+	};
 }
+
+
+
+
